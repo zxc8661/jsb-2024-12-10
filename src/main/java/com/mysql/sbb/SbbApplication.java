da5faa11331afa3490824efbc46dd47c06f3c71c
janjan <97529177+zxc8661@users.noreply.github.com>
2024-12-10 12:08:23 +0900

    엔티티로 테이블 매핑하기

--- src/main/java/com/mysql/sbb/SbbApplication.java
+++ src/main/java/com/mysql/sbb/SbbApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SbbApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SbbApplication.class, args);
 	}
 
