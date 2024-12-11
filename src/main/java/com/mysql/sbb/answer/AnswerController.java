5aec347dc6051573b9ea123ffbb82d64b042970f
janjan <97529177+zxc8661@users.noreply.github.com>
2024-12-11 17:44:09 +0900

    2-14

--- src/main/java/com/mysql/sbb/answer/AnswerController.java
+++ src/main/java/com/mysql/sbb/answer/AnswerController.java
@@ -16,10 +16,11 @@ public class AnswerController {
     private final QuestionService questionService;
 
 
-//    @PostMapping("/create/{id}")
-//    public String createAnswer(Model model, @PathVariable("id") Integer id, @RequestParam(value="content") String content){
-//        Question question = this.questionService.getQuestion(id);
-//        this.answerService.create(question,content);
-//        return String.format("redirect:/question/detail/%s",id);
-//    }
+    @PostMapping("/create/{id}")
+    public String createAnswer(Model model, @PathVariable("id") Integer id, @RequestParam(value="content") String content)
+    {
+        Question question = this.questionService.getQuestion(id);
+        this.answerService.create(question,content);
+        return String.format("redirect:/question/detail/%s",id);
+    }
 }
--- src/main/java/com/mysql/sbb/answer/AnswerService.java
+++ src/main/java/com/mysql/sbb/answer/AnswerService.java
@@ -14,11 +14,11 @@ public class AnswerService{
 
 
 
-//    public void create(Question question,String content){
-//        Answer answer = new Answer();
-//        answer.setContent(content);
-//        answer.setCreatedDate(LocalDateTime.now());
-//        answer.setQuestion(question);
-//        this.answerRepository.save(answer);
-//    }
+    public void create(Question question,String content){
+        Answer answer = new Answer();
+        answer.setContent(content);
+        answer.setCreateDate(LocalDateTime.now());
+        answer.setQuestion(question);
+        this.answerRepository.save(answer);
+    }
 }
