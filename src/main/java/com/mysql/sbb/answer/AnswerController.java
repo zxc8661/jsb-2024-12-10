7fec255836ee3246b13620688b9c1ea5d583ef94
janjan <97529177+zxc8661@users.noreply.github.com>
2024-12-12 22:14:04 +0900

    2-14까지 함

--- src/main/java/com/mysql/sbb/answer/AnswerController.java
+++ src/main/java/com/mysql/sbb/answer/AnswerController.java
@@ -1,3 +1,31 @@
 package com.mysql.sbb.answer;
 
 
+import com.mysql.sbb.question.Question;
+import com.mysql.sbb.question.QuestionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/answer")
+public class AnswerController {
+    private final QuestionService questionService;
+    private  final AnswerService answerService;
+
+
+    @PostMapping("/create/{id}")
+    public String createAnser(Model model,
+                              @PathVariable("id") Integer id,
+                              @RequestParam(value = "content") String content){
+        Question question = this.questionService.getQuestion(id);
+        this.answerService.create(question,content);
+        return String.format("redirect:/question/detail/%s",id);
+    }
+}
--- src/main/java/com/mysql/sbb/answer/AnswerService.java
+++ src/main/java/com/mysql/sbb/answer/AnswerService.java
@@ -1,3 +1,23 @@
 package com.mysql.sbb.answer;
 
 
+import com.mysql.sbb.question.Question;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+@Service
+public class AnswerService {
+    private final AnswerRepository answerRepository;
+
+    public void create(Question question,String content){
+        Answer answer = new Answer();
+        answer.setQuestion(question);
+        answer.setContent(content);
+        answer.setCreateDate(LocalDateTime.now());
+        this.answerRepository.save(answer);
+    }
+
+}
