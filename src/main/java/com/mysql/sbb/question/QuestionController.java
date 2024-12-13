102270ac91928dcbf4b3cad2846d7bb0679cf796
janjan <97529177+zxc8661@users.noreply.github.com>
2024-12-13 15:11:00 +0900

    3-2

--- src/main/java/com/mysql/sbb/question/QuestionController.java
+++ src/main/java/com/mysql/sbb/question/QuestionController.java
@@ -8,10 +8,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import com.mysql.sbb.question.QuestionForm;
 
 import java.util.List;
-import java.util.Optional;
 
 
 @RequestMapping("/question")
